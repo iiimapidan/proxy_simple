9526f88b7d320729b124503c324b2b284a25d223
imapidan <imapidan@outlook.com>
2022-03-04 00:25:36 +0800

    add sock5

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"log"
+	"net"
+)
+
 func main() {
+	// 启动sock5服务端
+	addr := "127.0.0.1:1081"
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Printf("无法监听 %v: %v", addr, err)
+	}
+
+	log.Printf("sock5:%v服务监听成功", addr)
+
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				continue
+			}
+		}
+	}()
 }
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,1 +1,12 @@
 package proxy
+
+import (
+	"io"
+	"net"
+)
+
+type Server interface {
+	Name() string
+	Addr() string
+	Handshake(conn net.Conn) (io.ReadWriter, error)
+}
